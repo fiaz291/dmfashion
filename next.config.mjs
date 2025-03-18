c152f55bacee808a493c0f08d964b44666689fed
Fiaz Ul Haq <sarimhijjazi@gmail.com.com>
2025-03-19 00:40:24 +0500

    add static export

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export", // Enables static export
+  images: {
+    unoptimized: true, // Disables Next.js image optimization (required for Netlify)
+  },
 };
 
 export default nextConfig;
